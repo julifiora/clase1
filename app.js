39174c1a9de0b2d92ac38557b1cd59b9be14cc28
Julian <julicallejero_30@hotmail.com>
2021-05-21 21:45:31 -0300

    hicimos calculadora

---
+++ app.js
@@ -0,0 +1,25 @@
+//Estos son los comentarios 
+ /*
+ 1. Defino las variables
+ */  
+var num1;
+var num2;
+
+
+let suma;
+let resta;
+let division;
+let multiplicacion;
+
+const PI = 3.14159;
+
+/* 
+2. Funcion: es un metodo o proceso que se repite
+*/
+function inicio(){
+    num1 = prompt("Ingrese el primer número:");
+    num2 = prompt("Ingrese el segundo numero");
+    
+    console.log(num1);
+    console.log(num2);
+};
